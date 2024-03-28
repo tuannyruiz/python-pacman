f1b6530bbb3b43f1832fda0a68cc82c9121e9ad1
Tuanny Ruiz <tuannyruiz@gmail.com>
2024-03-28 09:59:48 -0400

    wrap up

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pac-Man
+
+This is a very simple pacman game implementation in Python.
+The goal of this project is to practice Python skills.
+https://www.udemy.com/course/python-102-developing-a-text-based-pac-man-game
--- ui.py
+++ ui.py
@@ -1,14 +1,55 @@
-# @ -> our hero
-# G -> ghosts
-#P -> pills
-# . -> empty spaces
-# | and - -> walls
+ui_wall = [
+    "......",
+    "......",
+    "......",
+    "......"
+]
+
+ui_ghost = [
+    " .-.  ",
+    "| OO| ",
+    "|   | ",
+    "'^^^' "
+]
+
+ui_pacman = [
+    " .--. ",
+    "/ _.-'",
+    "\\  '-.",
+    " '--' "
+]
+
+ui_empty = [
+    "      ",
+    "      ",
+    "      ",
+    "      "
+]
+
+ui_pill = [
+    "      ",
+    " .-.  ",
+    " '-'  ",
+    "      "
+]
+
 
 def ui_print(map):
     for row in map:
-        for column in row:
-            print(column, end='')
-        print('')
+        for piece in range(4):
+            for column in row:
+                if column == 'G':
+                    print(ui_ghost[piece], end='')
+                if column == 'P':
+                    print(ui_pill[piece], end='')
+                if column == '@':
+                    print(ui_pacman[piece], end='')
+                if column == '.':
+                    print(ui_empty[piece], end='')
+                if column == '-' or column == '|':
+                    print(ui_wall[piece], end='')
+
+            print("")
 
 def ui_key():
     return input()
